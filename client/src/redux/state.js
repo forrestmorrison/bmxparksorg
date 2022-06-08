ed49c333371eef7afcf93fa6f454d0a83a06a50b
forrestmorrison <forrestmorrison@gmail.com>
2022-06-07 23:38:49 -0500

    added container and redux files

---
+++ client/src/redux/state.js
@@ -0,0 +1,15 @@
+export default {
+    user: {
+        username: 'test-user-1',
+        email: 'test-user-1@example.com'
+    },
+    parks: [
+        {
+            "id":   1,
+            "name": "Rockstar Energy Bike Park",
+            "address": "12257 Kuykendahl Rd, Houston, TX 77067",
+            "hours": "10AM-9PM",
+            "type": "dirt, freestyle, race, street"
+        }
+    ]
+}
