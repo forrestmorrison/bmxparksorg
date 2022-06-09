5d1cec6d702ab7bf09245e4ea4678da49b489872
forrestmorrison <forrestmorrison@gmail.com>
2022-06-09 09:27:11 -0500

    updated parks component

--- client/src/components/parks.js
+++ client/src/components/parks.js
@@ -1,5 +1,22 @@
+import React from 'react'
+import { Container, Paper, Chip } from '@material-ui/core';
 
+const Park = (props) => {
+    const id = props.match.params.id
+    const car = props.parks.find(c => c.id === id)
 
-const parks = () => {
-    
+    return (
+        <Container maxWidth="sm" className="park-container">
+            <Paper className="park-paper">
+                <h2>{park.name}</h2>
+                {
+                    Object.keys(park).map((key, idx) => {
+                        return <Chip label={`${key}: ${park[key]}`}></Chip>
+                    })
+                }
+            </Paper>
+        </Container>
+    )
 }
+
+export default Park
