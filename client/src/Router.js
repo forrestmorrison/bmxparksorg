f2dd70c965c03674763ee4081cad30a8df8fee99
forrestmorrison <forrestmorrison@gmail.com>
2022-07-05 16:43:36 -0500

    added Router.js file

---
+++ client/src/Router.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Switch, Route } from 'react-router'
+
+const Router = () => {
+    return (
+        <Switch>
+
+        </Switch>
+    );
+};
+
+
+
+
+
+
+
+
+
+
+export default Router;
