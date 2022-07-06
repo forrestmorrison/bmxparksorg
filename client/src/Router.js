c68b5c9373dd60c1675c806294827faf5f73a5a9
forrestmorrison <forrestmorrison@gmail.com>
2022-07-05 23:48:49 -0500

    added containers to Router.js

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Switch, Route } from 'react-router'
+import Dashboard from './containers/Dashboard'
+import Park from './containers/Park'
 
 const Router = () => {
     return (
         <Switch>
-
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/park/:id" component={Park} />
         </Switch>
     );
 };
