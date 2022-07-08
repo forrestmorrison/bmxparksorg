ffe201708af1f752a5a982fc5e518ff30b197f9c
forrestmorrison <forrestmorrison@gmail.com>
2022-07-08 16:30:14 -0500

    added Provider & BrowserRouter, no errors 1 warning

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,20 @@
 import './App.css';
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import Banner from './components/Banner';
-import logo from './images/bmxlogo.jpg';
-import LogIn from './components/LogIn';
-import AddUser from './components/AddUser';
-import AddPark from './containers/AddPark';
-import Dashboard from './components/Dashboard';
 
 
-class App extends Component {
-  
-  state = {
-    // loaded: false
-  }
+function App () {
 
-  getBackendData = () => {
-    fetch('http://localhost:4004/users')
-      .then(response => console.log(response.json()))
-      .then((data) => {
-        console.log(data);
-      })
-  }
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
 
-  componentDidMount() {
-    this.getBackendData();
-    // this.setState({loaded: true})
-    this.initMap();
-  }
+      </BrowserRouter>
+    </Provider>
+  );
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.loaded !== this.state.loaded) {
-      
-  //   }
-  // }
-
-  initMap = () => {
-    const rockstarpark = { lat: 29.95862, lng: -95.42057};
-    let map = new window.google.maps.Map(document.getElementById("map"), {
-      center: { lat: 39, lng: -96 },
-      zoom: 5,
-    });
-    const marker = new window.google.maps.Marker({
-    position: rockstarpark,
-    map: map,
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <div className='banner'>
-            <img src={logo} alt="" id="bmxlogo" />
-            <Banner />
-          </div>
-          <div className='buttons'>
-            <LogIn />
-            <AddUser />
-          </div>
-        </header>
-        <div id="map">
-          
-        </div>
-        <main>
-
-        </main>
-      </div>
-    );
-  }
-  
 }
 
 export default App;
--- client/src/components/AddUser.js
+++ client/src/components/AddUser.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react'
-import { blue } from '@material-ui/core/colors'
 import {
     Button,
     TextField,
