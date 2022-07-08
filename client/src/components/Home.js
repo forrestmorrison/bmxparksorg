754e7a236487e2f2e03431c9e2cb0d5c7923e1d7
forrestmorrison <forrestmorrison@gmail.com>
2022-07-07 21:32:00 -0500

    added Home component & container

---
+++ client/src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card, CardContent, CardActions, Divider } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+const Home = (props) => {
+    return (
+        <div className="card-container">
+            {/* Change cars to props.cars and remove the cars.json import above */}
+            {props.parks.map((park, idx) => (
+                <Card key={idx} className="card">
+                    <CardContent className="text-gray">
+                        <span>{park.name.toUpperCase()}</span>
+                        <ul>
+                        <li>address: {park["address"]}</li>
+                        <li>website: {park["website"]}</li>
+                        <li>type: {park["type"]}</li>
+                        </ul>
+                    </CardContent>
+                    <Divider />
+                    <CardActions style={{ color: 'purple' }}>
+                        <Link to={`/park/${park.id}`}>See More Details</Link>
+                    </CardActions>
+                </Card>
+            ))}
+        </div>
+    )
+}
+
+export default Home
