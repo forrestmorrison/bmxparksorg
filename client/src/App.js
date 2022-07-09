c502332ff129dad106b20251784e44f5dd6c3052
forrestmorrison <forrestmorrison@gmail.com>
2022-07-08 19:38:00 -0500

    Added working Nav to Redux Appgit add -A! Needs styling, though.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import Navigation from './components/Navigation';
 
 
 function App () {
@@ -10,7 +11,7 @@ function App () {
   return (
     <Provider store={store}>
       <BrowserRouter>
-
+        <Navigation />
       </BrowserRouter>
     </Provider>
   );
