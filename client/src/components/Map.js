cecccf13075e079f502f05451cbd939d8a60f668
forrestmorrison <forrestmorrison@gmail.com>
2022-09-07 21:57:54 -0500

    updated Map component & deleted test components

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,43 +1,13 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { GoogleApiWrapper } from '@react-google-maps/api';
 
 class Map extends Component {
-  
-    state = {
-      // loaded: false
-    }
-  
 
-  
-    componentDidMount() {
-      this.getBackendData();
-      // this.setState({loaded: true})
-      this.initMap();
-    }
-  
 
-  
-    initMap = () => {
-      const rockstarpark = { lat: 29.95862, lng: -95.42057};
-      let map = new window.google.maps.Map(document.getElementById("map"), {
-        center: { lat: 39, lng: -96 },
-        zoom: 5,
-      });
-      const marker = new window.google.maps.Marker({
-      position: rockstarpark,
-      map: map,
-      });
-    }
-  
-    render() {
-      return (
-          <div id="map">
-            
-          </div>
-      );
-    }
-    
-  }
+}
 
 
 
-export default Map;
+
+export default Map
