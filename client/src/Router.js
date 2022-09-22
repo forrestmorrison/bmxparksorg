d61702b5a3e4bbadf20fb26f8892a88f46130111
forrestmorrison <forrestmorrison@gmail.com>
2022-09-21 23:28:21 -0500

    added LogIn to Router.js

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Route, Routes } from "react-router-dom";
+import LogIn from './components/LogIn';
 import Dashboard from './containers/Dashboard'
 import Park from './containers/Park'
 
 const Router = () => {
     return (
         <Routes>
+            <Route path="/login" component={LogIn} />
             <Route path="/park/:id" component={Park} />
             <Route path="/dashboard" component={Dashboard} />
         </Routes>
