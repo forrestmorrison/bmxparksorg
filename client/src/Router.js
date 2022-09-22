be19b3a36136803684f6921974cb92062f6f7692
forrestmorrison <forrestmorrison@gmail.com>
2022-09-22 14:43:44 -0500

    Added Home component to Router, no errors

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Route, Routes } from "react-router-dom";
+import Home from './components/Home';
 import LogIn from './components/LogIn';
 import Dashboard from './containers/Dashboard'
 import Park from './containers/Park'
@@ -7,6 +8,7 @@ import Park from './containers/Park'
 const Router = () => {
     return (
         <Routes>
+            <Route exact path="/" component={Home} />
             <Route path="/login" component={LogIn} />
             <Route path="/park/:id" component={Park} />
             <Route path="/dashboard" component={Dashboard} />
