7b2669d9d070ae8419124346f6259dfe89e24304
forrestmorrison <forrestmorrison@gmail.com>
2022-09-29 23:24:40 -0500

    updated Map component

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,10 +4,27 @@ import { GoogleApiWrapper } from '@react-google-maps/api';
 
 class Map extends Component {
 
+    componentDidMount() {
+        this.getBackendData();
+        // this.setState({loaded: true})
+        this.initMap();
+      }
+
+    initMap = () => {
+        const rockstarpark = { lat: 29.95862, lng: -95.42057};
+        let map = new window.google.maps.Map(document.getElementById("map"), {
+        center: { lat: 39, lng: -96 },
+        zoom: 5,
+        });
+        const marker = new window.google.maps.Marker({
+        position: rockstarpark,
+        map: map,
+        });
+    }
 
 }
 
 
 
 
-export default Map
+export default Map;
